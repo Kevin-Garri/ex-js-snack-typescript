58cb520d6fb98630f28fdbb45b8dd9d5e01e4883
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-06-13 21:26:59 +0200

    add: snack2

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,20 @@ if (typeof dato === 'string') {
   console.log('Tipo non supportato');
 }
 
-/*Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+/*Snack2 Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
 nome → stringa
 cognome → stringa
 annoNascita → numero
 sesso → Può essere solo "m" o "f".
 anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])*/
 
+type Dipendente = {
+  nome: string;
+  cognome: string;
+  annoNascita: number;
+  sesso: 'm' | 'f';
+  anniDiServizio: number[];
+}
 
 /*Snack 3
 Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
